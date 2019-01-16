441163038a9f6bd00322e5e7689893adff582ff3
Mackenson <mackensondorancy@yahoo.com>
2019-01-15 22:33:23 -0500

    updated!

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/api/form', (req, res) =>{
+app.post('/form', (req, res) =>{
   console.log(req.body);
 })
 
-const PORT = process.env.PORT || 8080;
+app.set('port', (process.env.PORT || 5000));
 
-app.listen(PORT, () => {
-  console.log(`server listening on port ${PORT}`);
+app.listen(app.get('port'), function() {
+  console.log('server is running on port' +  app.get('port'));
 })
