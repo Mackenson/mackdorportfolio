6f9b9e479107ac74b5fc489dc5369737c1fe0358
Mackenson <mackensondorancy@yahoo.com>
2019-01-27 19:05:26 -0500

    updated

--- index.js
+++ index.js
@@ -12,12 +12,8 @@ app.post('/form', (req, res) =>{
   console.log(req.body);
 })
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/public/index.html'));
-});
-
 app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), function() {
-  console.log('server is running on port' +  app.get('port'));
+  console.log('server is running on port ' +  app.get('port'));
 })
