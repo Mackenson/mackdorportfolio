b936e5a03169dc18e8fa0523babea9186fa12466
Mackenson <mackensondorancy@yahoo.com>
2019-02-28 21:14:07 -0500

    updated!

--- src/container/About.js
+++ src/container/About.js
@@ -11,31 +11,9 @@ import mack4 from'../img/mack4.jpg';
 class About extends Component {
 
   render() {
-    var myIndex = 0;
-// TODO: make this better
-  function show() {
-    var i;
-    var x = document.getElementsByClassName("slides");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}
-    // TODO: x[myIndex] is underfine
-      // x[myIndex-1].style.display = "block";
-    setTimeout(show, 2000); // Change image every 2 seconds
-  }
     return (
       <div>
         <Nav />
-        <div>
-          <div>
-            <img onClick={show()} className="slides" src={mack1} />
-            <img onClick={show()} className="slides" src={mack2} />
-            <img onClick={show()} className="slides" src={mack3} />
-            <img onClick={show()} className="slides" src={mack4} />
-          </div>
-        </div>
         <header>
           <h1 id="main-title">Makenson Dorancy</h1>
           <h4>About Me</h4>
@@ -45,7 +23,12 @@ class About extends Component {
               the backend, fetching data from databas, setting up api end point,
               saving user informations in database. I have a big passions
               for front-end. I enjoy design and empliments new features.
+              Outside of work I enjoy travel to new places and enjoy new foods.
             </p>
+            <div id='img-box'>
+              <img src={mack2} className='img-about' />
+              <img src={mack3} className='img-about' />
+              </div>
           </div>
         </header>
       </div>
