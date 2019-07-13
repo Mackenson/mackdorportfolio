96364f8f397034afbf87bc77a0c43f064d0f83e0
Mackenson <mackensondorancy@yahoo.com>
2019-07-13 19:37:09 -0400

    add proxy to package.json

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/public', express.static(path.join(__dirname, 'public')))
 
-app.post('/form', (req, res) =>{
+app.post('/api/contact', function(req, res){
   console.log(req.body);
 })
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 3001));
 
 app.listen(app.get('port'), function() {
   console.log('server is running on port ' +  app.get('port'));
