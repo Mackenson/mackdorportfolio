d988c3a695650a6603965a388af387357d972212
Mackenson <mackensondorancy@yahoo.com>
2019-07-17 19:50:09 -0400

    added reactStrap and boostrap orginaze project.js

--- src/container/Header.js
+++ src/container/Header.js
@@ -3,11 +3,9 @@ import '../css/Header.css';
 class Header extends Component {
   render() {
     return (
-      <div>
-        <header>
+      <div id='header-text'>
           <h1 className="title">{this.props.title}</h1>
           <a onClick={this.props.button}>Projects</a>
-        </header>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './container/App';
+import 'bootstrap/dist/css/bootstrap.css';
 import * as serviceWorker from './serviceWorker';
 // import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
