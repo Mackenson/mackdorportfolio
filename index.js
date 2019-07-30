6afd651c8f737d9b8f2a8e008fa2dea41065e8d6
Mackenson <mackensondorancy@yahoo.com>
2019-07-30 17:31:21 -0400

    updated

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const path = require('path');
 const morgan = require('morgan');
 const compression = require('compression');
 const createServer = require('http');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 
 dotenv.config();
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/public', express.static(path.join(__dirname, 'public')))
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 const dev = app.get('env') !== 'production';
 
@@ -31,9 +31,43 @@ if (dev) {
   app.use(morgan('dev'));
 }
 
-app.post('/form', (req, res) =>{
-  console.log(req.body);
-});
+app.post('/api/contact', function(req, res){
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main(){
+
+  const htmlEmail = `
+    <h3>Contact Info</h3>
+    <ul>
+      <li>Name:${req.body.name}</li>
+      <li>Email:${req.body.email}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `;
+
+  let testAccount = await nodemailer.createTestAccount();
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.ETHEREAL_USERNAME, // generated ethereal user
+      pass: process.env.ETHEREAL_PASS // generated ethereal password
+    }
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: `"Portfolio 👻" <${req.body.email}>`, // sender address
+    to: "rowan.torp@ethereal.email", // list of receivers
+    subject: "New Message ✔", // Subject line
+    text: `${req.body.message}`, // plain text body
+    html: `<b>${req.body.message}</b>` // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
 
   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 }
