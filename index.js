37893463d4e5963f6876643fd3806a6baf61108e
Mackenson <mackensondorancy@yahoo.com>
2019-07-30 16:40:59 -0400

    install morgan and compression

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
-var path = require('path');
+const path = require('path');
+const morgan = require('morgan');
+const compression = require('compression');
+const createServer = require('http');
+
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+const dev = app.get('env') !== 'production';
+
+if (!dev) {
+  app.disable('x-powered-by');
+  app.use(compression());
+  app.use(morgan('common'));
+
+  app.use(express.static(path.resolve(__dirname, 'build')));
+
+  app.get('*', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, 'build', 'index.js'));
+  });
+}
+
+if (dev) {
+  app.use(morgan('dev'));
+}
+
 app.post('/form', (req, res) =>{
   console.log(req.body);
-})
+});
 
 
 
@@ -18,4 +40,4 @@ app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), function() {
   console.log('server is running on port ' +  app.get('port'));
-})
+});
